e0efae7e8ebc7e0806370e10ae79da841b8f2e89
QuickRecon <aren.leishman@gmail.com>
2018-08-11 17:20:20 +0800

    make some more bits and link it all up

--- layouts/main.vue
+++ layouts/main.vue
@@ -26,20 +26,20 @@
 
                     <li class="luxbar-item"><a href="/whypodi">Why Dive PODI?</a></li>
 
-                    <li class="luxbar-item"><a href="#">Learn to Dive</a></li>
-                    <li class="luxbar-item dropdown"><a href="#">Specialties</a>
+                    <li class="luxbar-item"><a href="/learntodive">Learn to Dive</a></li>
+                    <li class="luxbar-item dropdown"><a href="/courses">Specialties</a>
                         <ul>
-                            <li class="luxbar-item" ><a href="#">Boat</a></li>
-                            <li class="luxbar-item" ><a href="#">Cave</a></li>
-                            <li class="luxbar-item" ><a href="#">Martian</a></li>
-                            <li class="luxbar-item" ><a href="#">Motorcycle</a></li>
-                            <li class="luxbar-item" ><a href="#">Nitrox</a></li>
-                            <li class="luxbar-item" ><a href="#">Wreck</a></li>
+                            <li class="luxbar-item" ><a href="/courses/boat">Boat</a></li>
+                            <li class="luxbar-item" ><a href="/courses/cave">Cave</a></li>
+                            <li class="luxbar-item" ><a href="/courses/martian">Martian</a></li>
+                            <li class="luxbar-item" ><a href="/courses/motorcycle">Motorcycle</a></li>
+                            <li class="luxbar-item" ><a href="/courses/nitrox">Nitrox</a></li>
+                            <li class="luxbar-item" ><a href="/courses/wreck">Wreck</a></li>
                         </ul>
                     </li>
-                    <li class="luxbar-item"><a href="#">PODI Pro</a></li>
-                    <li class="luxbar-item"><a href="#">TechWreck</a></li>
-                    <li class="luxbar-item"><a href="#">About Us</a></li>
+                    <li class="luxbar-item"><a href="/pro">PODI Pro</a></li>
+                    <li class="luxbar-item"><a href="/techwreck">TechWreck</a></li>
+                    <li class="luxbar-item"><a href="/about">About Us</a></li>
                 </ul>
             </div>
         </header>
@@ -55,9 +55,9 @@
                     <div class="col l4 offset-l2 s12">
                         <h5 class="white-text">What we offer:</h5>
                         <ul>
-                            <li><a class="grey-text text-lighten-3" href="#!">Courses</a></li>
-                            <li><a class="grey-text text-lighten-3" href="#!">PODI Pro</a></li>
-                            <li><a class="grey-text text-lighten-3" href="#!">TechWreck</a></li>
+                            <li><a class="grey-text text-lighten-3" href="/courses">Courses</a></li>
+                            <li><a class="grey-text text-lighten-3" href="/pro">PODI Pro</a></li>
+                            <li><a class="grey-text text-lighten-3" href="/techwreck">TechWreck</a></li>
                         </ul>
                     </div>
                 </div>
---
+++ pages/pro.vue
@@ -0,0 +1,25 @@
+<style>
+</style>
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        layout: 'main',
+        data () {
+            return {
+                title: 'Dive PODI'
+            }
+        },
+        head () {
+            return {
+                title: this.title,
+                meta: [
+                ]
+            }
+        }
+    }
+</script>
